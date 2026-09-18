ec1c71af4f2bc501d1b95e3a7655aa2d0634940c
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 13:29:46 +0530

    string question 2

--- practiceques3.py
+++ practiceques3.py
@@ -1,4 +1,11 @@
+<<<<<<< Updated upstream
 #wap to check the type of data entered by the user using input() function.
 
 a = input ( "enter value of a :-")
 print( type ( a ))
+=======
+#wap to check the type of data entered by the user using input() function.
+
+a = input ( "enter value of a :-")
+print( type ( a ))
+>>>>>>> Stashed changes
---
+++ strings/stringques2.py
@@ -0,0 +1,4 @@
+letter = '''dear <|Name|>,
+you are selected!
+<|date|> '''
+print(letter.replace("<|Name|>","Priyanshu").replace("<|date|>","23 sep 2029"))
