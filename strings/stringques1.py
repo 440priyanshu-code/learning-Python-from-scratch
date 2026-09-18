ce4f4f813eea0eb6b40dcb54fa4b313bda4e505f
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 13:05:17 +0530

    string question 1

---
+++ strings/stringques1.py
@@ -0,0 +1,5 @@
+#wap to display a user entered name followed by good afternoon using input() function.
+
+name= (input( " enter your name:- "))
+print(f"good afternoon {name}")
+#print("good afternoon"  +name + "") this is also valid.
