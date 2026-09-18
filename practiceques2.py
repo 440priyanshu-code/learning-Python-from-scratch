8712a0d346c344179d0b0fb848f491e21fbc41e4
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 11:49:14 +0530

    Add files via upload

---
+++ practiceques2.py
@@ -0,0 +1,6 @@
+#wap to find the remainder when a number is divided by z
+num1 = int ( input ( " enter any number :- "))
+num2 = int ( input ( " enter any number :- "))
+
+rem =  num1 % num2 
+print( "Remainder when num1 is divisible by num2 is ", num1 % num2 )
