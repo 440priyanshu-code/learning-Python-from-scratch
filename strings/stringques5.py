7f21dc3b2f462ec99e14303e08fa9c09f5da6618
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 16:56:32 +0530

    string question 5

---
+++ strings/stringques5.py
@@ -0,0 +1,6 @@
+#wap to format the following letters using an escape sequence characters.
+#letter = "Dear priyanshu, you are doing great. thanks!"
+
+letter = "Dear priyanshu, you are doing great. thanks!"
+
+print("dear priyanshu \n  you are doing great\n \t thanks! ")
