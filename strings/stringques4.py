e5a4075f015a89facbf076334796aade65a90977
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 16:47:05 +0530

    string question 4

--- strings/stringques4.py
+++ strings/stringques4.py
@@ -1,3 +1,4 @@
 #wap to replace the double space in any sentence with a single space.
-name = "my name  is priyanshu mishra"
-print( name.find("  "))
+name = "my name  is Priyanshu mishra i am happy"
+print(name.replace("  "," "))
+
