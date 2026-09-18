e0502863fce9c5d30c43581e9729f3e5e30b0a4b
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-18 12:20:34 +0530

    Add files via upload

---
+++ practiceques5.py
@@ -0,0 +1,7 @@
+#wap to print avg of two numbers entered by the user.
+num1 = int( input ( " enter any number:- "))
+num2 = int( input ( " enter any number:- "))
+
+avg = (num1+num2)/2
+
+print ( avg )
