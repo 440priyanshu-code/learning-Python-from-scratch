147c4ecfa4678adeb81ce0d83d61f9ed8121ef61
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-19 10:33:30 +0530

    lsitquestion1

---
+++ list&tuples/listques1.py
@@ -0,0 +1,18 @@
+#wap to store seven fruits in a lsit enetered by a user.
+fruits =  []
+f1 = input ( "enter any fruits name :" )
+fruits.append(f1)
+f2 = input ( "enter any fruits name :" )
+fruits.append(f2)
+f3= input ( "enter any fruits name :" )
+fruits.append(f3)
+f4 = input ( "enter any fruits name :" )
+fruits.append(f4)
+f5 = input ( "enter any fruits name :" )
+fruits.append(f5)
+f6 = input ( "enter any fruits name :" )
+fruits.append(f6)
+f7 = input ( "enter any fruits name :" )
+fruits.append(f7)
+
+print(fruits)
