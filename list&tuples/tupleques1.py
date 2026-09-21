3fcd63de8c671359a5b0687bc15325432a1463e1
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-21 21:21:39 +0530

    tuple question

--- list&tuples/tupleques1.py
+++ list&tuples/tupleques1.py
@@ -7,4 +7,8 @@ a[0] = 10
 print(a)
 
 #ERROR!
+<<<<<<< Updated upstream
 #TypeError: 'tuple' object does not support item assignment
+=======
+#TypeError: 'tuple' object does not support item assignment
+>>>>>>> Stashed changes
