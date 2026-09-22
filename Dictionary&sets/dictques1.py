afa606c989b1e020e0caddd927e48f653035ad48
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-22 22:26:58 +0530

    dictionary  question

---
+++ Dictionary&sets/dictques1.py
@@ -0,0 +1,12 @@
+# wap to create a dictionary of hindi words with values as thire english meaning .
+# provide user with an option to look it up!
+
+words = {
+    "madad" : "help",
+    "kursi" : "chair", 
+    "billi" : "cat",
+}
+
+word =input ("enter the word you want meaning of :")
+
+print ( words[ word ])
