72b9a89be120bd8100379c6682cc627b71f6f667
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-22 23:01:33 +0530

    Update setques3.py

--- Dictionary&sets/setques3.py
+++ Dictionary&sets/setques3.py
@@ -11,3 +11,5 @@ s.add(20.0)
 s.add('20')
 print (s)
 print(len(s))
+
+#OUTPUT WILL BE 3 NOT 2 bcz in python the comparision operator ( ==) jsut checks they are numerically equal or not and if they are it says true simillary it happend here in sets.
