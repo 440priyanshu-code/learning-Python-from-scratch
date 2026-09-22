fd61d7f45ee3ab87520e4d0efdf0410cc1c01502
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-22 22:38:50 +0530

    set question

---
+++ Dictionary&sets/setques1.py
@@ -0,0 +1,21 @@
+#wap to input 8 numbers from the user and display all the unique numbers (once).
+
+s= set()  #empty set 
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+n = input( "enter any number: ")
+s.add(int (n))
+
+print (s)
