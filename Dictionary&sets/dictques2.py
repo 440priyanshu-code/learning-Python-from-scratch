b68e4ca45b0c97c49d280ff3e7821aa020e0fcc6
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-23 15:18:35 +0530

    dictionary question

--- Dictionary&sets/dictques2.py
+++ Dictionary&sets/dictques2.py
@@ -1,5 +1,12 @@
 #s = {} what is its type .
 
 s = {}
+<<<<<<< HEAD
 print ( type( s))
 #OUTPUT :- <class 'dict'>  NOT SET
+=======
+print ( type(s))
+
+#OUTPUT :- <class 'dict'>  NOT SET
+
+>>>>>>> fb817a1 (dictionary question)
---
+++ Dictionary&sets/dictques3.py
@@ -0,0 +1,22 @@
+# create an empty dictionary.allow 4 friends to enter their 
+#favorite language as value and use key as their names. Assume that the names are unique.
+
+d ={}
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input("enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+print(d)
+
