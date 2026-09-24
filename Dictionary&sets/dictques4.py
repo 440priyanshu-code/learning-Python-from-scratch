ccf40ab5a1d40c8dc564a41c987b018bdeeb9296
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-24 19:22:36 +0530

    dictionary question

---
+++ Dictionary&sets/dictques4.py
@@ -0,0 +1,22 @@
+#what will happen when two names are same in previous question ?
+ 
+
+
+d ={}
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input("enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+print(d)
