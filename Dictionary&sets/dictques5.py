d95cf060c674871210e333e0b7be48d995555ecc
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-25 10:55:52 +0530

    dictionary question

---
+++ Dictionary&sets/dictques5.py
@@ -0,0 +1,36 @@
+# what will happen when two languages are same in previous question ?
+
+
+d ={}
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input("enter faourite language :")
+d.update({name : lang})
+
+name = input( "enter friends name:")
+lang = input( "enter faourite language :")
+d.update({name : lang})
+
+print(d)
+
+#OUTPUT:-
+
+# PS D:\internship\pyhton\Dictionary&sets> python dictques5.py
+# enter friends name:shivam 
+# enter faourite language :python
+# enter friends name:pragya 
+# enter faourite language :python
+# enter friends name:priyanshu
+# enter faourite language :JS 
+# enter friends name:mishra 
+# enter faourite language :C++
+
+# {'shivam ': 'python', 'pragya ': 'python', 'priyanshu': 'JS ', 'mishra ': 'C++'}
+
