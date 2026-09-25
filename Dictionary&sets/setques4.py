34a1540202a0745bdb9ea7335b6da82674ee1274
Priyanshu Mishra <440priyanshu@gmail.com>
2026-09-25 11:07:47 +0530

    set question

---
+++ Dictionary&sets/setques4.py
@@ -0,0 +1,14 @@
+#change the values inside the a list which is contained in set S 
+#s= {8,7,12,"HARRY", [1,2]}
+
+s= {8,7,12,"HARRY", [1,2]}
+
+
+
+# A list cannot be contained inside a set
+# because lists are mutable and unhashable.
+
+s = {8, 7, 12, "HARRY", [1, 2]}
+
+# answer: You cannot change values inside a list contained in a set, because Python doesn't allow a list to be an element of a set in the first place.
+
